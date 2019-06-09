34a1539a5f434f1bfdbe8eee231301ac1c8596c0
Stuart Axon <stuaxo2@yahoo.com>
2019-06-09 01:05:53 +0100

    Make intiail test pass. Add intiial .editorconfig

--- tests/test_toybot_script.py
+++ tests/test_toybot_script.py
@@ -3,9 +3,9 @@ from textwrap import dedent
 from toybot.languages.helpers import run
 
 
-def test_toybot_script():
+def test_toybot_script(capsys):
     """
-    Small end to end test.
+    Small end to end test using the ConsoleRender to confirm commands get rendered.
     """
     code = dedent(
         """
@@ -13,5 +13,14 @@ def test_toybot_script():
         rect(25, 30, 50, 70)
         """
     )
+    expected = dedent(
+        """\
+        {'type': 'path', 'coords': [(0, 0), (0, 100), (100, 100), (100, 0)]}
+        {'type': 'path', 'coords': [(25, 30), (25, 100), (75, 100), (75, 30)]}
+        """
+    )
 
     run(code)
+    captured = capsys.readouterr()
+
+    assert captured.out == expected

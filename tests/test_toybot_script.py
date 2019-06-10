3136080408840f8af179dac93a21611e3c439e0b
Stuart Axon <stuaxo2@yahoo.com>
2019-06-10 01:07:58 +0100

    Update pyproject.toml for Black. Small formatting change in test.

--- tests/test_toybot_script.py
+++ tests/test_toybot_script.py
@@ -8,7 +8,7 @@ def test_toybot_script(capsys):
     Small end to end test using the ConsoleRender to confirm commands get rendered.
     """
     code = dedent(
-        """
+        """\
         rect(0, 0, 100, 100)
         rect(25, 30, 50, 70)
         """
